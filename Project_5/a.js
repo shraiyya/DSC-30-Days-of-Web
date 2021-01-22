e2ef8b3da2c96896611049fa196c26e6e6d5e8fb
Shreya Pawaskar <shreyap2550@gmail.com>
2021-01-22 17:05:06 +0530

    Add files via upload

---
+++ Project_5/a.js
@@ -0,0 +1,142 @@
+var c=0
+var a1=0
+var a2=0
+function submit_form() {
+var c2=0;
+  c++
+var table = document.getElementById("input-form");
+var name=document.getElementById('name').value 
+var sub=document.getElementById('subject').value 
+var email=document.getElementById('email').value 
+var message=document.getElementById('message').value 
+var row_length=table.rows.length;
+if(c2!=1){  
+  var row = table.insertRow(c);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  var cell4 = row.insertCell(3);
+cell1.innerHTML=name;
+cell2.innerHTML=sub;
+cell3.innerHTML=email;
+cell4.innerHTML=message;
+  }
+}
+
+
+
+function setError(input, errormsg) {
+	const formGroup = input.parentElement;
+	const inputAlert = formGroup.querySelector(".input-alert");
+	formGroup.className = "form-group error";
+	inputAlert.innerText = errormsg;
+}
+
+
+function setSuccess(input) {
+	const formGroup = input.parentElement;
+	formGroup.className = "form-group success";
+}
+
+
+function validEmail(email) {
+	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
+}
+
+
+function validateForm(form) {
+	if (form.fname.value.trim() === "") {
+		setError(form.fname, "name cannot be blank");
+		return false;
+	} else {
+		setSuccess(form.fname);
+	}
+	
+	if (form.subject.value.trim() === "") {
+		setError(form.subject, "subject cannot be blank");
+		return false;
+	} else {
+		setSuccess(form.subject);
+	}
+
+	if (form.email.value.trim() === "") {
+		setError(form.email, "Email cannot be blank");
+		return false;
+	} else if (!validEmail(form.email.value.trim())) {
+		setError(form.email, "Email is not valid");
+		return false;
+	} else {
+		setSuccess(form.email);
+	}
+
+	if (form.message.value.trim() === "") {
+		setError(form.message, "Message cannot be blank");
+		return false;
+	} else {
+		setSuccess(form.message);
+	}
+
+	return true;
+}
+
+
+
+
+
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.09em solid #fef}";
+        document.body.appendChild(css);
+    };
+
